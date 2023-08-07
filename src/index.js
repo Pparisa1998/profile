9d51e32d654eba594b6159be7a1e2fef610e0f11
Pparisa1998 <parisa.na.65@ubu.ac.th>
2023-08-07 19:11:17 +0700

    profile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,96 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function toThaiDateString(date) {
+  let monthNames = [
+    "มกราคม",
+    "กุมภาพันธ์",
+    "มีนาคม",
+    "เมษายน",
+    "พฤษภาคม",
+    "มิถุนายน",
+    "กรกฎาคม",
+    "สิงหาคม.",
+    "กันยายน",
+    "ตุลาคม",
+    "พฤศจิกายน",
+    "ธันวาคม",
+  ];
+
+  let year = date.getFullYear() + 543;
+  let month = monthNames[date.getMonth()];
+  let numOfDay = date.getDate();
+
+  let hour = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let second = date.getSeconds().toString().padStart(2, "0");
+
+  return `${numOfDay} ${month} ${year} ${hour}:${minutes}:${second} น.`;
+}
+
+function MyDateTime() {
+  let date1 = new Date();
+  date1 = toThaiDateString(date1);
+  return <div style={{ margin: "40px" }}>{date1}</div>;
+}
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+          <SkillList />
+        </div>
+      </div>
+      <MyDateTime />
+    </>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="Adobe Illustrator" color="#F9E5F6" />
+      <Skill skill="Adobe Photoshop" color="#D549D4" />
+      <Skill skill="Adobe AfterEffect" color="#EBBBE3" />
+      <Skill skill="Adobe Premiere pro" color="#9A6CE1" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>พริสา นภากร</h1>
+      <p> นักศึกษาปริญญษโท สาขาเทคโนโลยีสารสนเทศ </p>
+      <p> และการสื่อสาร คณะวิทยาศาสตร์ มหาวิทยาลัยอุบลราชธานี </p>
+      <p> ชื่นชอบในการนอน ขอบคุณค่ะ</p>
+      <p>
+        <h3> warning : อย่าให้เขียนโคเด </h3>
+      </p>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="risa.png" alt="P A R I S A" />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
